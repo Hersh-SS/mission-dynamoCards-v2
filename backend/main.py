c88f60e247d8b8c3730904a92a00170591280738
Hersh-SS <hersheysandhu@gmail.com>
2024-06-22 03:44:47 -0400

    Finished Task 3

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+"""
 from fastapi import FastAPI
 from pydantic import BaseModel, HttpUrl
 from fastapi.middleware.cors import CORSMiddleware
@@ -49,4 +50,39 @@ def analyze_video(request: VideoAnalysisRequest):
     
     return {
         "key_concepts": key_concepts_list
+    }
+"""
+
+from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl
+
+class VideoAnalysisRequest(BaseModel):
+    youtube_link: HttpUrl
+    # advanced settings
+
+app = FastAPI()
+
+@app.post("/analyze_video")
+def analyze_video(request: VideoAnalysisRequest):
+    # Doing the analysis
+    from langchain_community.document_loaders import YoutubeLoader
+    from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+    loader = YoutubeLoader.from_youtube_url(str(request.youtube_link), add_video_info=True)
+    docs = loader.load()
+    print(f"On load: {type(docs)}")
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    result = text_splitter.split_documents(docs)
+
+    print(f"{type(result)}")
+    author = result[0].metadata['author']
+    length = result[0].metadata['length']
+    title = result[0].metadata['title']
+    total_size = len(result)
+
+    return {
+        "author": author,
+        "length": length,
+        "title": title,
+        "total_size": total_size
     }
