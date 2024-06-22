5b6465ef2fe31f2811d6b680c9a1b99dff13e107
Hersh-SS <hersheysandhu@gmail.com>
2024-06-22 13:23:33 -0400

    Finished Task 5

--- backend/services/genai.py
+++ backend/services/genai.py
@@ -1,3 +1,4 @@
+"""
 from langchain_community.document_loaders import YoutubeLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_vertexai import VertexAI
@@ -102,12 +103,13 @@ class YoutubeProcessor:
             # Prompt for finding concepts 
             prompt = PromptTemplate(
                 template = """
+'''
                 Find and define key concepts or terms found in the text:
                 {text}
                 
                 Respond in the following format as a JSON object without any backticks separating each concept with a comma:
                 {{"concept": "definition", "concept": "definition", ...}}
-                """,
+'''                """,
                 input_variables=["text"]
             )
             
@@ -141,4 +143,37 @@ class YoutubeProcessor:
         
         logging.info(f"Total Analysis Cost: ${batch_cost}")    
         return processed_concepts
-    
+"""
+
+from langchain_community.document_loaders import YoutubeLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import logging
+
+# Configure log
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class YoutubeProcessor:
+    # Retrieve the full transcript
+
+    def __init__(self):
+        self.text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = 1000,
+            chunk_overlap = 0
+        )
+
+    def retrieve_youtube_documents(self, video_url: str, verbose = False):
+        loader = YoutubeLoader.from_youtube_url(video_url, add_video_info=True)
+        docs = loader.load()
+        result = self.text_splitter.split_documents(docs)
+        
+
+        author = result[0].metadata['author']
+        length = result[0].metadata['length']
+        title = result[0].metadata['title']
+        total_size = len(result)
+
+        if verbose:
+            logger.info(f"{author}\n{length}\n{title}\n{total_size}")
+
+        return result
